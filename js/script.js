32410b3937e12e6012f3a760b2c940b4e5fc4d63
jordanbolivarq <60189623+jordanbolivarq@users.noreply.github.com>
2022-05-18 05:07:46 -0500

    a

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function abrirMenu() {
+	const menu = document.getElementById('menuHaborguesa');
+	menu.classList.add('show')
+}
+//const menu_ = document.getElementsById('menuNav');
+//menu_.addEventListener('click', abrirMenu());
